ee4736c0a26a60aebc5308daa73534fb8dc207a4
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-13 11:37:28 +0200

    php mazaa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <h1>What is a PHP File?</h1>
+  <ul>
+    <li>PHP files can contain text, HTML, CSS, JavaScript, and PHP code</li>
+    <li>PHP code is executed on the server, and the result is returned to the browser as plain HTML</li>
+    <li>PHP files have extension ".php"</li>
+  </ul>
+  <h1>What Can PHP Do?</h1>
+  <ul>
+    <li>PHP can generate dynamic page content</li>
+    <li>PHP can create, open, read, write, delete, and close files on the server</li>
+    <li>PHP can collect form data</li>
+    <li>PHP can send and receive cookies</li>
+    <li>PHP can add, delete, modify data in your database</li>
+    <li>PHP can be used to control user-access</li>
+    <li>PHP can encrypt data</li>
+  </ul>
+  <p>With PHP you are not limited to output HTML. You can output images or PDF files. You can also output any text, such as XHTML and XML.</p>
+  </>
   );
 }
 
